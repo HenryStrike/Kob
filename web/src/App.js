259f8bdb71f13a15155203a1222ec4e050fea69c
Che Hongle <chehongle123@163.com>
2022-07-30 10:14:38 +0800

    add check local user function and remove message action

--- web/src/App.js
+++ web/src/App.js
@@ -5,48 +5,20 @@ import 'bootstrap/dist/js/bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRoutes } from 'react-router-dom';
 import Routes from './components/Routes';
-import ACTIONS from './actions/types';
-import { useEffect } from 'react';
-import userActions from './actions/userActions';
+import validationActions from './actions/validationActions';
 
 function App() {
 	const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+    const isPulling = useSelector((state) => state.user.isPulling);
 	const routing = useRoutes(Routes(isLoggedIn));
 	const dispatch = useDispatch();
 
-	useEffect(()=>{
-        // check user validation
-        const token = localStorage.getItem("token");
-        if(token) {
-            // update token
-            dispatch({
-                type : ACTIONS.UPDATE_TOKEN,
-                payload : token,
-            });
-            // check token validation
-            dispatch(userActions.getInfo({
-                token,
-                success(){
-                    // show view
-                    dispatch({
-                        type : ACTIONS.UPDATE_PULLING,
-                        payload : false,
-                    });
-                },
-                error(){
-                    dispatch({
-                        type : ACTIONS.UPDATE_PULLING,
-                        payload : false,
-                    });
-                },
-            }));
-        }else{
-            dispatch({
-                type : ACTIONS.UPDATE_PULLING,
-                payload : false,
-            });
-        }
-    },[dispatch]);
+    if(!isLoggedIn && isPulling){
+        dispatch(validationActions.CheckLocalUser({
+            success(){},
+            error(){},
+        }));
+    }
 
 	return (
 		<div className="App">
